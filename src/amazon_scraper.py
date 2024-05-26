4b337a4f2f2e13720306928952352683438df700
charuka97 <charukatharindhu@gmail.com>
2024-05-26 08:48:04 +0530

    Values not availabale issue fixed

--- src/amazon_scraper.py
+++ src/amazon_scraper.py
@@ -2,13 +2,15 @@ import src.utils as utils
 from src.database import MongoDB, get_config
 import random
 import re
+
 config = get_config()
 
 
-def parse_amazon_product_page(soup):
+def parse_amazon_product_page(soup, productCategory):
 
+    #  s-result-list
     products = []
-    product_containers = soup.find_all("div", class_="s-result-item")
+    product_containers = soup.find_all("div", class_="puis-card-container")
 
     for container in product_containers:
         product = {}
@@ -37,26 +39,23 @@ def parse_amazon_product_page(soup):
 
         # Get product images
         image_elements = container.find_all("img", class_="s-image")
-        product["images"] = (
-            [img["src"] for img in image_elements]
-            if image_elements
-            else ["Not available"]
-        )
+        image_urls = []
+
+        for img in image_elements:
+            srcset = img.get("srcset", "")
+            urls = [url.strip() for url in srcset.split(",")]
+            image_urls.extend(urls)
+        product["images"] = image_urls
 
         # Get product category
-        category_element = soup.find("span", class_="a-color-state")
-        product["category"] = (
-            category_element.text.strip().strip('"/')
-            if category_element
-            else "Not available"
-        )
+        product["category"] = productCategory
 
         products.append(product)
 
     return products
 
 
-def scrape_amazon_category(category_url):
+def scrape_amazon_category(category_url, productCategory):
 
     db = MongoDB()
     base_url = config["amazon"]["base_url"]
@@ -64,16 +63,18 @@ def scrape_amazon_category(category_url):
 
     user_agent = config["amazon"]["User_Agents"]
     headers = config["amazon"]["headers"]
-    
+
     auth_content["User-Agent"] = random.choice(user_agent)
     auth_content.update(headers)
 
     soup = utils.fetch_page(category_url, auth_content)
     if soup:
         product_data = utils.handle_pagination(
-            soup, base_url, auth_content, parse_amazon_product_page
+            soup,
+            base_url,
+            auth_content,
+            lambda soupObj: parse_amazon_product_page(soupObj, productCategory),
         )
         for product in product_data:
             db.insert_item(config["database"]["collections"]["amazon"], product)
     db.close()
-
